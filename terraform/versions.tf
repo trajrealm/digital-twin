4683cf189da5cfb8c7c82129efc7844ef6789530
trajrealm <trajrealm@trajrealm.com>
2026-04-24 10:51:11 -0400

    Initial commit: Digital Twin infrastructure and application

---
+++ terraform/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Uses AWS CLI configuration (aws configure)
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
